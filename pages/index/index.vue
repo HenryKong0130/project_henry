d985dc4f44930b9b1bb5920bce42dc1f21319895
妖娆的小胖子 <henrykong123456@gmail.com>
2023-11-15 23:26:47 +0800

    完成了项目的初始化工作

---
+++ pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="container">
+		<view class="">left</view>
+		<view class="">center</view>
+		<view class="">right</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style scoped lang="scss">
+.container{
+	@include flex(space-between,row)
+	color:$base-color
+}
+</style>
